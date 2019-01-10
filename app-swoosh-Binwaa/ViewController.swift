8a427c732ed24a563eb7bb3d125053204e56cac6
BenoitPhilips <Benoit.Philips@GMail.com>
2019-01-10 16:49:32 +0100

    Lesson 29 : first exercise about auto layout

--- app-swoosh-Binwaa/ViewController.swift
+++ app-swoosh-Binwaa/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var WelcomeScreenBg: UIImageView!
+    @IBOutlet weak var swooshBg: UIImageView!
+    @IBOutlet weak var goAllInLbl: UILabel!
+    @IBOutlet weak var allOutLbl: UILabel!
+    @IBOutlet weak var findTeamLbl: UILabel!
+    @IBOutlet weak var getStartedBtn: BorderButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+//        getStartedBtn.frame = CGRect(x: view.frame.size.width / 2 - getStartedBtn.frame.size.width / 2, y: view.frame.size.height - 100, width: getStartedBtn.frame.size.width, height: getStartedBtn.frame.size.height)
+//        swooshBg.frame = view.frame
+        
+        
+        
     }
 
     // enkel commentraar om iets te wijzigen
